e6172ed127008551ee81cf9bfa71d25c26f38e48
saifalkindi <qe2d@hotmail.com>
2023-01-21 05:31:25 +0400

    First Version

--- app/src/main/java/com/saif/gogopharmacy/MainActivity.java
+++ app/src/main/java/com/saif/gogopharmacy/MainActivity.java
@@ -1,14 +1,36 @@
 package com.saif.gogopharmacy;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // hide the action bar
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LogIn.class));
+                finish();
+            }
+        }, 3000);
     }
 }
