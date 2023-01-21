a5cc4886909748f17a5e3af62a60672595c71e9d
saifalkindi <qe2d@hotmail.com>
2023-01-22 00:54:37 +0400

    Second Version

--- app/src/main/java/com/saif/gogopharmacy/MainActivity.java
+++ app/src/main/java/com/saif/gogopharmacy/MainActivity.java
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -17,20 +22,27 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int splash_screen = 4000;
+    private  ImageView logo_Image;
+    private Animation logo_anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        // hide the action bar
-        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(MainActivity.this, LogIn.class));
                 finish();
-            }
-        }, 3000);
+            }}, splash_screen);
+
+        // set the animation to the image
+        logo_Image = findViewById(R.id.imageView);
+        logo_anim = AnimationUtils.loadAnimation(this, R.anim.logo_anim);
+        logo_Image.startAnimation(logo_anim);
     }
 }
